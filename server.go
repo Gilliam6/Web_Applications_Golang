17070559555501d434e874345c432bac7fdfe5ef
Gilliam6 <wizzzardoz@icloud.com>
2022-09-04 01:16:56 +0400

    Final project commit

--- server.go
+++ server.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 type Page struct {
 	Title string
@@ -27,11 +32,64 @@ func loadPage(fileName string) (*Page, error) {
 	return &Page{Title: fileName, Body: body}, nil
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	// err = t.Execute(w, p)
+	//
+	//	if err != nil {
+	//		http.Error(w, err.Error(), http.StatusInternalServerError)
+	//	}
 }
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+	//fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+	//fmt.Fprintf(w, "<h1>Editing %s</h1>"+"<form action=\"/save/%s\" method=\"POST\">"+
+	//	"<textarea name=\"body\">%s</textarea><br>"+
+	//	"<input type=\"submi\" value=\"Save\">"+
+	//	"</form>", p.Title, p.Title, p.Body)
+
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
 func main() {
 	//	Execrsice 1
 	//	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
@@ -40,6 +98,10 @@ func main() {
 	//	fmt.Println(string(p2.Body))
 
 	//Exe 2:
-	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+
+	fmt.Println("Server is UP listening port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
